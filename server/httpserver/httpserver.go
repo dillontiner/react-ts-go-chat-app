513ae557ed42369a66b3b1697efefc1be6187631
Dillon Tiner <dillontiner@gmail.com>
2022-11-17 19:58:10 -0800

    move httpserver

---
+++ server/httpserver/httpserver.go
@@ -0,0 +1,147 @@
+package httpserver
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"server/api"
+	"server/entities"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+type Server struct {
+	API *api.Client
+}
+
+func NewServer(api *api.Client) Server {
+	return Server{
+		API: api,
+	}
+}
+
+func (s *Server) ServeHTTP(port int) {
+	logger := log.New(os.Stdout, "HTTP Server: ", log.LstdFlags)
+
+	//
+	// HTTP Server and MiddleWare
+	//
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // TODO: env var for this
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	//
+	// HTTP Routes
+	//
+	r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		splitAuthHeader := strings.Split(authHeader, "Basic ")
+		encodedEmailPassword := splitAuthHeader[1]
+		decodedEmailPassword, err := base64.StdEncoding.DecodeString(encodedEmailPassword)
+
+		if err != nil {
+			// failed parsing auth
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		emailPassword := strings.SplitN(string(decodedEmailPassword), ":", 2)
+		if len(emailPassword) != 2 {
+			// failed parsing auth
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		user := entities.User{
+			Email:    emailPassword[0],
+			Password: emailPassword[1],
+		}
+		createdUser, err := s.API.CreateUser(user)
+		if err != nil {
+			if err.Error() == "ERROR: duplicate key value violates unique constraint \"users_email_key\" (SQLSTATE 23505)" { // hacky error handling
+				http.Error(w, http.StatusText(400), 400) // TODO: pass this to FE in interpretable way
+				return
+			}
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		response := entities.LoginResponse{
+			UUID: createdUser.UUID,
+		}
+
+		json.NewEncoder(w).Encode(response)
+	})
+
+	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		splitAuthHeader := strings.Split(authHeader, "Basic ")
+		encodedEmailPassword := splitAuthHeader[1]
+		decodedEmailPassword, err := base64.StdEncoding.DecodeString(encodedEmailPassword)
+
+		if err != nil {
+			// failed parsing auth
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		emailPassword := strings.SplitN(string(decodedEmailPassword), ":", 2)
+		if len(emailPassword) != 2 {
+			// failed parsing auth
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		email := emailPassword[0]
+		password := emailPassword[1]
+
+		userUUID, err := s.API.AuthorizeUser(email, password)
+		if err != nil {
+			if err.Error() == "unauthorized" {
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			} else {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+
+			return
+		}
+
+		response := entities.LoginResponse{
+			UUID: *userUUID,
+		}
+
+		json.NewEncoder(w).Encode(response)
+	})
+
+	r.Get("/chat", func(w http.ResponseWriter, r *http.Request) {
+		messages, err := s.API.GetMessages()
+		logger.Println("err here?")
+		logger.Println(messages, err)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		response := entities.GetChatResponse{
+			Messages: *messages,
+		}
+
+		json.NewEncoder(w).Encode(response)
+	})
+
+	formattedPort := fmt.Sprintf(":%v", port)
+	http.ListenAndServe(formattedPort, r)
+}
--- server/main.go
+++ server/main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"server/api"
 	"server/entities"
+	"server/httpserver"
 	"server/websocketserver"
-	"strings"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -24,133 +19,12 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	logger := log.New(os.Stdout, "server: ", log.LstdFlags)
+	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
 
 	apiClient, err := api.NewClient()
 	if err != nil {
 		panic(err)
 	}
-
-	//
-	// HTTP Server and MiddleWare
-	//
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"http://localhost:3000"}, // TODO: env var for this
-		AllowedMethods: []string{"GET", "POST"},
-		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		// ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
-
-	//
-	// HTTP Routes
-	//
-
-	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte("welcome"))
-	// })
-
-	r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
-		// TODO: abstract decoding
-		authHeader := r.Header.Get("Authorization")
-		splitAuthHeader := strings.Split(authHeader, "Basic ")
-		encodedEmailPassword := splitAuthHeader[1]
-		decodedEmailPassword, err := base64.StdEncoding.DecodeString(encodedEmailPassword)
-
-		if err != nil {
-			// failed parsing auth
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		emailPassword := strings.SplitN(string(decodedEmailPassword), ":", 2)
-		if len(emailPassword) != 2 {
-			// failed parsing auth
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		user := entities.User{
-			Email:    emailPassword[0],
-			Password: emailPassword[1],
-		}
-		createdUser, err := apiClient.CreateUser(user)
-		if err != nil {
-			if err.Error() == "ERROR: duplicate key value violates unique constraint \"users_email_key\" (SQLSTATE 23505)" { // hacky error handling
-				http.Error(w, http.StatusText(400), 400) // TODO: pass this to FE in interpretable way
-				return
-			}
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		response := entities.LoginResponse{
-			UUID: createdUser.UUID,
-		}
-
-		json.NewEncoder(w).Encode(response)
-	})
-
-	r.Get("/login", func(w http.ResponseWriter, r *http.Request) {
-		// TODO: abstract decoding
-		authHeader := r.Header.Get("Authorization")
-		splitAuthHeader := strings.Split(authHeader, "Basic ")
-		encodedEmailPassword := splitAuthHeader[1]
-		decodedEmailPassword, err := base64.StdEncoding.DecodeString(encodedEmailPassword)
-
-		if err != nil {
-			// failed parsing auth
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		emailPassword := strings.SplitN(string(decodedEmailPassword), ":", 2)
-		if len(emailPassword) != 2 {
-			// failed parsing auth
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		email := emailPassword[0]
-		password := emailPassword[1]
-
-		userUUID, err := apiClient.AuthorizeUser(email, password)
-		if err != nil {
-			if err.Error() == "unauthorized" {
-				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-			} else {
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			}
-
-			return
-		}
-
-		response := entities.LoginResponse{
-			UUID: *userUUID,
-		}
-
-		json.NewEncoder(w).Encode(response)
-	})
-
-	r.Get("/chat", func(w http.ResponseWriter, r *http.Request) {
-		messages, err := apiClient.GetMessages()
-		logger.Println("err here?")
-		logger.Println(messages, err)
-		if err != nil {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		response := entities.GetChatResponse{
-			Messages: *messages,
-		}
-
-		json.NewEncoder(w).Encode(response)
-	})
 
 	voteInMemoryQueue := make(chan entities.Vote)
 	go func() {
@@ -177,5 +51,6 @@ func main() {
 	}()
 
 	logger.Println("HTTP Server running on port 4000")
-	http.ListenAndServe(":4000", r)
+	httpServer := httpserver.NewServer(apiClient)
+	httpServer.ServeHTTP(4000)
 }
